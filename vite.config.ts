1ec9ecb26bc52e5984f5714e22befb19625e5030
Александр <alex056com@gmail.com>
2025-08-26 16:01:31 +0300

    Update config and package

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/project/", // ⚠️ замените на имя вашего репозитория
+  base: '/project/', // ⚠️ замените на имя вашего репозитория
 });
